c7befccfb36392f92fa94431e0f2afd2215be582
Soujanya R M <soujanya@atsgen.com>
2020-02-28 05:10:26 +0000

    Adding tf-blueprint script

---
+++ tf-blueprint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source setup-env.sh
+cat objects.yaml.env | envsubst > objects.yaml
+cat TF_blueprint.yaml.env | envsubst > TF_blueprint.yaml
+cd ..
+export PATH=$PATH:$PWD/api-server/scripts
+cd tf/
+rc_loaddata -H $RC_HOST -u $RC_USER -p $RC_PW -A objects.yaml
+rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW blueprint create TF_blueprint.yaml
+export ESID=$(rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW edgesite list | awk 'NR==3 {print $1}')
+export BPID=$(rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW blueprint list | awk 'NR==3 {print $1}')
+cat POD.yaml.env | envsubst > POD.yaml
+rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW pod create POD.yaml
