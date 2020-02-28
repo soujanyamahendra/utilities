324402efcf2f6036ca5a867b436314159229c28b
soujanyamahendra <43268191+soujanyamahendra@users.noreply.github.com>
2020-02-28 10:32:35 +0530

    Update tf-blueprint.sh

--- tf-blueprint.sh
+++ tf-blueprint.sh
@@ -9,7 +9,5 @@ rc_loaddata -H $RC_HOST -u $RC_USER -p $RC_PW -A objects.yaml
 rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW blueprint create TF_blueprint.yaml
 export ESID=$(rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW edgesite list | awk 'NR==3 {print $1}')
 export BPID=$(rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW blueprint list | awk 'NR==3 {print $1}')
-echo "ESID = $ESID"
-echo "BPID = $BPID"
 cat POD.yaml.env | envsubst > POD.yaml
 rc_cli -H $RC_HOST -u $RC_USER -p $RC_PW pod create POD.yaml
